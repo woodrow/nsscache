f9b1d45dcfe0abf17098f60c3c5c398610ca39ed
Stephen Woodrow <srwoodrow@gmail.com>
2015-02-27 21:35:08 -0800

    Sort of working.

--- nss_cache/sources/conjursource.py
+++ nss_cache/sources/conjursource.py
@@ -27,8 +27,6 @@ import datetime
 import calendar
 import logging
 import time
-import ldap
-import ldap.sasl
 import urllib
 
 from nss_cache import error
@@ -49,7 +47,7 @@ class ConjurSource(source.Source):
 
   """
   # conjur connection defaults
-  CONJUR_URI = None
+  CONJUR_URI = ''
   CONJUR_USERNAME = ''
   CONJUR_API_KEY = ''
   CONJUR_ACCOUNT = 'conjur'
@@ -81,43 +79,42 @@ class ConjurSource(source.Source):
     if client is None:
       # ReconnectLDAPObject should handle interrupted ldap transactions.
       # also, ugh
-      config = conjur.config.Config({
-          'appliance_url': conf['conjur_uri'],
-          'account': conf['conjur_account'],
-          'verify_ssl': conf['conjur_verify_ssl'],
-          'cert_file': conf['conjur_cert_file'],
-          })
-      client = conjur.new_from_key(conf['conjur_username'],
-                                   conf['conjur_api_key'],
+      config = conjur.config.Config(
+          appliance_url=conf['uri'],
+          account=conf['account'],
+          verify_ssl=False,#conf['verify_ssl'],
+          cert_file=conf['cert_file'],
+          )
+      client = conjur.new_from_key(conf['username'],
+                                   conf['api_key'],
                                    config)
-    else:
-      self.client = client
+    self.client = client
 
   def _SetDefaults(self, configuration):
     """Set defaults if necessary."""
 
     # conjur client params
-    if not 'conjur_uri' in configuration:
-      configuration['conjur_uri'] = self.CONJUR_URI
-    if not configuration['conjur_uri'].startswith('https://'):
+    if not ('uri' in configuration and configuration['uri']):
+      configuration['uri'] = self.CONJUR_URI
+    if not configuration['uri'].startswith('https://'):
         raise error.ConfigurationError('conjur_uri must start with https://')
 
-    if not 'conjur_username' in configuration:
-      configuration['conjur_username'] = self.CONJUR_USERNAME
-    if not 'conjur_api_key' in configuration:
-      configuration['conjur_api_key'] = self.CONJUR_API_KEY
-    if not 'conjur_account' in configuration:
-      configuration['conjur_account'] = self.CONJUR_ACCOUNT
-    if not 'conjur_verify_ssl' in configuration:
-      configuration['conjur_verify_ssl'] = self.CONJUR_VERIFY_SSL
-    if not 'conjur_cert_file' in configuration:
-      configuration['conjur_cert_file'] = self.CONJUR_CERT_FILE
+    if not 'username' in configuration:
+      configuration['username'] = self.CONJUR_USERNAME
+    if not 'api_key' in configuration:
+      configuration['api_key'] = self.CONJUR_API_KEY
+    if not 'account' in configuration:
+      configuration['account'] = self.CONJUR_ACCOUNT
+    if not 'verify_ssl' in configuration:
+      configuration['verify_ssl'] = self.CONJUR_VERIFY_SSL
+    if not 'cert_file' in configuration:
+      configuration['cert_file'] = self.CONJUR_CERT_FILE
 
     # conjur group search params
-    if not 'conjur_group_posix_users' in configuration:
-      configuration['conjur_group_posix_users'] = self.CONJUR_GROUP_POSIX_USERS
-    if not 'conjur_group_posix_groups' in configuration:
-      configuration['conjur_group_posix_groups'] = self.CONJUR_GROUP_POSIX_GROUPS
+    if not 'group_posix_users' in configuration:
+      configuration['group_posix_users'] = self.CONJUR_GROUP_POSIX_USERS
+    if not 'group_posix_groups' in configuration:
+      configuration['group_posix_groups'] = self.CONJUR_GROUP_POSIX_GROUPS
 
   def GetSshkeyMap(self, since=None):
     """Return the sshkey map from this source.
@@ -136,11 +133,11 @@ class ConjurSource(source.Source):
             (datetime.datetime.utcnow() - self.pwmap_cache_last_fetch) <
             datetime.timedelta(minutes=5)):
         pwmap = self.pwmap_cache
-    else
+    else:
         pwmap = self.GetPasswdMap(since)
 
     for pw in pwmap:
-        keys = [k for k in api.public_keys(pw.name).split('\n') if len(k) > 0]
+        keys = [k for k in self.client.public_keys(pw.name).split('\n') if len(k) > 0]
         for k in keys:
             skey = sshkey.SshkeyMapEntry()
             skey.name = pw.name
@@ -161,18 +158,18 @@ class ConjurSource(source.Source):
     """
 
     pwmap = passwd.PasswdMap()
-    members = self.client.group(self.conf.conjur_group_posix_users).members()
+    members = self.client.group(self.conf['group_posix_users']).members()
     users = self.filter_group_members_by_kind(members, 'user')
     for u in users:
-        parts = u.split(':')
+        parts = u['member'].split(':')
         assert(parts[0] == 'stripe' and parts[1] == 'user' and len(parts) == 3)
-        user = api.user(parts[2]))
-        assert(user.exists())
+        conjur_user = self.client.user(parts[2])
+        assert(conjur_user.exists())
         pw = passwd.PasswdMapEntry()
-        pw.name = user.login
-        pw.uid = user.uidnumber
-        pw.gid = user.uidnumber  # this is a hack
-        pw.gecos = user.login  # needs attrs
+        pw.name = conjur_user.login
+        pw.uid = conjur_user.uidnumber
+        pw.gid = conjur_user.uidnumber  # this is a hack
+        pw.gecos = conjur_user.login  # needs attrs
         pw.shell = '/bin/bash'  # needs attrs
         pw.dir = os.path.join('/pay/home/', pw.name)  # needs attrs
         pw.passwd = '!'
@@ -184,9 +181,9 @@ class ConjurSource(source.Source):
 
     return pwmap
 
-  def filter_group_members_by_kind(self, members, kind)
+  def filter_group_members_by_kind(self, members, kind):
       filtered = []
-      resource_prefix = '{}:{}:'.format(self.conf.conjur_account, kind)
+      resource_prefix = '{}:{}:'.format(self.conf['account'], kind)
       for m in members:
           if m['member'].startswith(resource_prefix):
               filtered.append(m)
@@ -203,15 +200,15 @@ class ConjurSource(source.Source):
       instance of maps.GroupMap
     """
     gmap = group.GroupMap()
-    members = self.client.group(self.conf.conjur_group_posix_groups).members()
+    members = self.client.group(self.conf['group_posix_groups']).members()
     groups = self.filter_group_members_by_kind(members, 'group')
     for g in groups:
-        parts = g.split(':')
+        parts = g['member'].split(':')
         assert(parts[0] == 'stripe' and parts[1] == 'group' and len(parts) == 3)
-        group = api.group(parts[2]))
+        conjur_group = self.client.group(parts[2])
         #assert(group.exists())
         gr = group.GroupMapEntry()
-        gr.name = group.id
+        gr.name = conjur_group.id
         gr.gid = 50000  # needs attrs
         gr.members = []
         gr.passwd = '!'
@@ -228,211 +225,3 @@ class ConjurSource(source.Source):
 #      since = int(time.time() + 60)
 #    results = self.GetPasswdMap(since=since)
 #    return len(results)
-
-
-class UpdateGetter(object):
-  """Base class that gets updates from LDAP."""
-
-  def FromLdapToTimestamp(self, ldap_ts_string):
-    """Transforms a LDAP timestamp into the nss_cache internal timestamp.
-
-    Args:
-      ldap_ts_string: An LDAP timestamp string in the format %Y%m%d%H%M%SZ
-
-    Returns:
-      number of seconds since epoch.
-    """
-    t = time.strptime(ldap_ts_string, '%Y%m%d%H%M%SZ')
-    return int(calendar.timegm(t))
-
-  def FromTimestampToLdap(self, ts):
-    """Transforms nss_cache internal timestamp into a LDAP timestamp.
-
-    Args:
-      ts: number of seconds since epoch
-
-    Returns:
-      LDAP format timestamp string.
-    """
-    t = time.strftime('%Y%m%d%H%M%SZ', time.gmtime(ts))
-    return t
-
-  def GetUpdates(self, source, search_base, search_filter,
-                 search_scope, since):
-    """Get updates from a source.
-
-    Args:
-      source: a data source
-      search_base: the LDAP base of the tree
-      search_filter: the LDAP object filter
-      search_scope:  the LDAP scope filter, one of 'base', 'one', or 'sub'.
-      since: a timestamp to get updates since (None for 'get everything')
-
-    Returns:
-      a tuple containing the map of updates and a maximum timestamp
-
-    Raises:
-      error.ConfigurationError: scope is invalid
-      ValueError: an object in the source map is malformed
-    """
-    self.attrs.append('modifyTimestamp')
-
-    if since is not None:
-      ts = self.FromTimestampToLdap(since)
-      # since openldap disallows modifyTimestamp "greater than" we have to
-      # increment by one second.
-      ts = int(ts.rstrip('Z')) + 1
-      ts = '%sZ' % ts
-      search_filter = ('(&%s(modifyTimestamp>=%s))' % (search_filter, ts))
-
-    if search_scope == 'base':
-      search_scope = ldap.SCOPE_BASE
-    elif search_scope == 'one':
-      search_scope = ldap.SCOPE_ONELEVEL
-    elif search_scope == 'sub':
-      search_scope = ldap.SCOPE_SUBTREE
-    else:
-      raise error.ConfigurationError('Invalid scope: %s' % search_scope)
-
-    source.Search(search_base=search_base, search_filter=search_filter,
-                  search_scope=search_scope, attrs=self.attrs)
-
-    # Don't initialize with since, because we really want to get the
-    # latest timestamp read, and if somehow a larger 'since' slips through
-    # the checks in main(), we'd better catch it here.
-    max_ts = None
-
-    data_map = self.CreateMap()
-
-    for obj in source:
-      for field in self.essential_fields:
-        if field not in obj:
-          logging.warn('invalid object passed: %r not in %r', field, obj)
-          raise ValueError('Invalid object passed: %r', obj)
-
-      obj_ts = self.FromLdapToTimestamp(obj['modifyTimestamp'][0])
-
-      if max_ts is None or obj_ts > max_ts:
-        max_ts = obj_ts
-
-      try:
-        if not data_map.Add(self.Transform(obj)):
-          logging.info('could not add obj: %r', obj)
-      except AttributeError, e:
-        logging.warning('error %r, discarding malformed obj: %r',
-                        str(e), obj)
-
-    data_map.SetModifyTimestamp(max_ts)
-
-    return data_map
-
-
-class PasswdUpdateGetter(UpdateGetter):
-  """Get passwd updates."""
-
-  def __init__(self):
-    super(PasswdUpdateGetter, self).__init__()
-    self.attrs = ['userid', 'uidnumber']
-    self.annotations = ['gidNumber', 'gecos', 'homeDirectory', 'loginShell',
-            'fullName']
-    self.essential_annotations = ['gidNumber', 'homeDirectory']
-
-  def CreateMap(self):
-    """Returns a new PasswdMap instance to have PasswdMapEntries added to it."""
-    return passwd.PasswdMap()
-
-  def Transform(self, obj):
-    """Transforms a LDAP posixAccount data structure into a PasswdMapEntry."""
-
-    pw = passwd.PasswdMapEntry()
-
-    if 'gecos' in obj:
-      pw.gecos = obj['gecos'][0]
-    elif 'cn' in obj:
-      pw.gecos = obj['cn'][0]
-    elif 'fullName' in obj:
-      pw.gecos = obj['fullName'][0]
-    else:
-      raise ValueError('Neither gecos nor cn found')
-
-    pw.gecos = pw.gecos.replace('\n','')
-
-    pw.name = obj['uid'][0]
-    if 'loginShell' in obj:
-      pw.shell = obj['loginShell'][0]
-    else:
-      pw.shell = ''
-
-    pw.uid = int(obj['uidNumber'][0])
-    pw.gid = int(obj['gidNumber'][0])
-    pw.dir = obj['homeDirectory'][0]
-
-    # hack
-    pw.passwd = 'x'
-
-    return pw
-
-
-class GroupUpdateGetter(UpdateGetter):
-  """Get group updates."""
-
-  def __init__(self,conf):
-    super(GroupUpdateGetter, self).__init__()
-    if conf.has_key('rfc2307bis') and conf['rfc2307bis']:
-      self.attrs = ['cn', 'gidNumber', 'member']
-    else:
-      self.attrs = ['cn', 'gidNumber', 'memberUid']
-    self.essential_fields = ['cn']
-
-  def CreateMap(self):
-    """Return a GroupMap instance."""
-    return group.GroupMap()
-
-  def Transform(self, obj):
-    """Transforms a LDAP posixGroup object into a group(5) entry."""
-
-    gr = group.GroupMapEntry()
-
-    gr.name = obj['cn'][0]
-    # group passwords are deferred to gshadow
-    gr.passwd = '*'
-    members = []
-    if 'memberUid' in obj:
-      members.extend(obj['memberUid'])
-    elif 'member' in obj:
-      for member_dn in obj['member']:
-        member_uid = member_dn.split(',')[0].split('=')[1]
-        members.append(member_uid)
-    members.sort()
-
-    gr.gid = int(obj['gidNumber'][0])
-    gr.members = members
-
-    return gr
-
-
-class SshkeyUpdateGetter(UpdateGetter):
-  """Fetches SSH keys."""
-
-  def __init__(self):
-    super(SshkeyUpdateGetter, self).__init__()
-    self.attrs = ['uid', 'sshPublicKey']
-    self.essential_fields = ['uid']
-
-  def CreateMap(self):
-    """Returns a new SshkeyMap instance to have SshkeyMapEntries added to it."""
-    return sshkey.SshkeyMap()
-
-  def Transform(self, obj):
-    """Transforms a LDAP posixAccount data structure into a SshkeyMapEntry."""
-
-    skey = sshkey.SshkeyMapEntry()
-
-    skey.name = obj['uid'][0]
-
-    if 'sshPublicKey' in obj:
-      skey.sshkey = obj['sshPublicKey']
-    else:
-      skey.sshkey = ''
-
-    return skey
